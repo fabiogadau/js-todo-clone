a9ca8af0967df9b84be29f6f7d570e05fa19dc1d
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-16 14:05:42 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,51 @@
+/*
+* Todo List
+Ricreare la todo list vista oggi durante la lezione (focus dell’esercizio la logica)
+Usiamo i template in html e il clone per separare la parte logica (JS) dalla parte di markup (HTML)
+recap funzionalità: mostrare, aggiungere e eliminare elementi dalla lista
+*/
+
+$( document ).ready(function() {
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+});
