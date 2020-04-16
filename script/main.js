e8cebcdc9955b13649be42d0a2e08f877e534e70
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-16 16:17:18 +0200

    Added possibility for the user to highlight special reminders

--- script/main.js
+++ script/main.js
@@ -10,29 +10,44 @@ $( document ).ready(function() {
   // Referenze
   var list = $('.todo');
   var newInput = $('.add-reminder');
+  var app = $('#app');
 
   // Aggiunta di un nuovo reminder alla Todo List
   newInput.keyup(function(event) {
+    
+    // Al sollevamento del tasto Invio viene eseguito il seguente codice
     if ( event.which == 13 || event.keyCode == 13 ) {
       var text = newInput.val().trim();
       console.log(text);
 
+      // Validazione e aggiunta reminder alla Todo List
       if ( text !== '' ) {
         var newReminder = $('.template li').clone();
         newReminder.prepend(text);
         list.append(newReminder);
 
+        // Reset dell'input
         newInput.val('');
       }
     }
   });
 
-  $('#app').on('click', '.todo li .fa-trash', function() {
-    $(this).parent().remove();
+  // Cancellazione reminder dalla Todo List
+  app.on('click', '.todo li .fa-trash', function() {
+    var icons = $(this).parent();
+    icons.parent().remove();
   });
 
-  $('#app').on('click', '.todo li', function() {
+  // Aggiunta text-decoration al reminder completato
+  app.on('click', '.todo li', function() {
     $(this).toggleClass('done');
+  });
+
+  // Aggiunta possibilità per l'utente di evidenziare dei reminder speciali
+  app.on('click', '.todo li .fa-star', function() {
+    var icons = $(this).parent();
+    icons.parent().toggleClass('special');
+    $(this).toggleClass('special-color');
   });
 
 
